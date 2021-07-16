231161a919b9f7e54877b469e8fdcd5345550071
梁胖 <liangpang@SH-liangpang-5.local>
2021-07-16 10:39:59 +0800

    v1

--- server_client/client.go
+++ server_client/client.go
@@ -2,10 +2,50 @@ package server_client
 
 import (
 	"fmt"
+	"github.com/golang/glog"
 	"github.com/lemontree2015/skynet"
 	"github.com/lemontree2015/skynet.common.min/gproto"
 	"github.com/lemontree2015/skynet/client"
+	"github.com/lemontree2015/skynet/config"
+	"strconv"
+	"strings"
 )
+
+var (
+	GIMServerServiceKeys []*skynet.ServiceKey // GIM Server[%v] Service
+)
+
+func init() {
+	// 初始化GIMServerServiceKeys
+	if str, err := config.DefaultString("gim_server.indexs"); err == nil {
+		indexs := strings.Split(str, ":")
+		serviceKeys := make([]*skynet.ServiceKey, 0, 0)
+		for _, s := range indexs {
+			if s == "" {
+				continue
+			}
+
+			if v, err := strconv.ParseInt(s, 10, 64); err == nil {
+				if v >= 0 {
+					name := fmt.Sprintf("GIM Server[%v] Service", v)
+					serviceKey := skynet.NewServiceKey(name, "1.0.0")
+					serviceKeys = append(serviceKeys, serviceKey)
+				} else {
+					panic(fmt.Errorf("gim_server.indexs Format Error: %v", str))
+				}
+			} else {
+				panic(fmt.Errorf("gim_server.indexs Format Error: %v", str))
+			}
+
+			GIMServerServiceKeys = serviceKeys
+		}
+	} else {
+		panic(fmt.Errorf("Can't Find gim_server.indexs"))
+	}
+
+	glog.Infof("GIMServerServiceKeys=%v", GIMServerServiceKeys)
+
+}
 
 ////////////////////
 // RPC UserCount
